08a5e4fe3351665ee4429a2762a19137f587dbf6
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-11-21 05:01:40 +0100

    Apply formatting

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,105 @@
-"use strict";
+"use strict"
 
-import decache from "decache";
+import decache from "decache"
 
-decache("handlebars");
+decache("handlebars")
 import { create as createDefaultHandlebars } from "handlebars"
-decache("handlebars");
+decache("handlebars")
 
 import { safeJsonParse } from "better-error-message-for-json-parse"
-import { IHandlebars, ICompileOptions } from "./makeHandlebarsBehave";
+import { IHandlebars, ICompileOptions } from "./makeHandlebarsBehave"
 
 export function escapeJson(str: any): string {
-    str = str ? str.toString() : '';
-    return str
-        .replace(/\\/g, "\\\\")
-        .replace(/\t/g, "\\t")
-        .replace(/\n/g, "\\n")
-        .replace(/"/g, '\\"')
-        .replace(/\r/g, "\\r");
+  str = str ? str.toString() : ""
+  return str
+    .replace(/\\/g, "\\\\")
+    .replace(/\t/g, "\\t")
+    .replace(/\n/g, "\\n")
+    .replace(/"/g, '\\"')
+    .replace(/\r/g, "\\r")
 }
 
 export interface IOptions {
-    numberOfDebugLinesInError: number;
+  numberOfDebugLinesInError: number
 }
 
 export const defaultNumberOfDebugLines = Object.freeze({
-    numberOfDebugLinesInError: 3
-});;
+  numberOfDebugLinesInError: 3
+})
 
 /**
  * Creates a JSON version of handlebars.
- * 
+ *
  * @export
- * @param {IOptions} [options=defaultNumberOfDebugLines] Option to inflence errors.
+ * @param {IOptions} [options=defaultNumberOfDebugLines] Option to influence errors.
  * @returns {IHandlebars} The Handlebars object.
  */
 export function createJsonHandlebars(options: IOptions = defaultNumberOfDebugLines): IHandlebars {
-    const instance = createDefaultHandlebars();
-    instance.Utils.escapeExpression = escapeJson;
-    (<any>instance).create = createJsonHandlebars;
-    (<any>instance).__def__compile = instance.compile;
-    (<any>instance).compile = (input: any, options?: ICompileOptions) => {
-        const compiled = (<any>instance).__def__compile(input, options);
-        return (context: any, options: any): any => {
+  const instance = createDefaultHandlebars()
+  instance.Utils.escapeExpression = escapeJson
+  ;(<any>instance).create = createJsonHandlebars
+  ;(<any>instance).__def__compile = instance.compile
+  ;(<any>instance).compile = (input: any, options?: ICompileOptions) => {
+    const compiled = (<any>instance).__def__compile(input, options)
+    return (context: any, options: any): any => {
+      let result = compiled(context, options)
+      result = reformatJsonString(result)
 
-            let result = compiled(context, options);
-            result = reformatJsonString(result);
-
-            try {
-                return safeJsonParse(result);
-            }
-            catch (ex) {
-                ex = changeError(ex, result, options);
-                throw ex;
-            }
-        };
+      try {
+        return safeJsonParse(result)
+      } catch (ex) {
+        ex = changeError(ex, result, options)
+        throw ex
+      }
     }
-    return instance;
+  }
+  return instance
 }
 
-function reformatJsonString(str) {
+function reformatJsonString(str: string) {
+  // replace enters
+  str = str.replace(/\r/g, "")
 
-    // replace enters
-    str = str.replace(/\r/g, '');
+  // skip empty line
+  str = str
+    .split("\n")
+    .filter(l => !/^\s*$/.test(l))
+    .join("\n")
 
-    // skip empty line
-    str = str.split('\n').filter(l => !/^\s*$/.test(l)).join('\n');
+  // replace lines that only have a comma - they are hard to debug
+  str = str.replace(/\n\s*,\s*\n/g, ",\n")
 
-    // replace lines that only have a comma - they are hard to debug
-    str = str.replace(/\n\s*,\s*\n/g, ',\n');
-
-    return str;
+  return str
 }
 
 function changeError(ex: Error, json: string, options: IOptions) {
+  const DIFF = options ? (options.numberOfDebugLinesInError ? options.numberOfDebugLinesInError : 3) : 3
 
-    const DIFF = options ? options.numberOfDebugLinesInError ? options.numberOfDebugLinesInError : 3 : 3;
+  let jsons = json.split("\n")
+  let messages = ex.message.split("\n")
+  let firstMsg = messages.shift()
 
-    let jsons = json.split('\n');
-    let messages = ex.message.split('\n');
-    let firstMsg = messages.shift();
+  let firstRangeEnd = parseInt(messages[1]) - 2
+  let firstRangeStart = firstRangeEnd - DIFF
+  if (firstRangeStart < 0) firstRangeStart = 0
 
-    let firstRangeEnd = parseInt(messages[1]) - 2;
-    let firstRangeStart = firstRangeEnd - DIFF;
-    if (firstRangeStart < 0) firstRangeStart = 0;
+  for (let index = firstRangeStart; index < firstRangeEnd; index++) {
+    let msg = `${index + 1}: ${jsons[index]}`
+    let offset = index - firstRangeStart
+    messages.splice(offset, 0, msg)
+  }
 
-    for (let index = firstRangeStart; index < firstRangeEnd; index++) {
-        let msg = `${index + 1}: ${jsons[index]}`;
-        let offset = index - firstRangeStart;
-        messages.splice(offset, 0, msg);
-    }
+  let lastRangeStart = parseInt(messages[messages.length - 1])
+  let lastRangeEnd = lastRangeStart + DIFF
+  if (lastRangeEnd >= jsons.length) lastRangeEnd = jsons.length - 1
 
-    let lastRangeStart = parseInt(messages[messages.length - 1]);
-    let lastRangeEnd = lastRangeStart + DIFF;
-    if (lastRangeEnd >= jsons.length) lastRangeEnd = jsons.length - 1;
+  for (let index = lastRangeStart; index <= lastRangeEnd; index++) {
+    let msg = `${index + 1}: ${jsons[index]}`
+    messages.push(msg)
+  }
 
-    for (let index = lastRangeStart; index <= lastRangeEnd; index++) {
-        let msg = `${index + 1}: ${jsons[index]}`;
-        messages.push(msg);
-    }
+  messages.unshift(firstMsg)
+  ex.message = messages.join("\n")
 
-    messages.unshift(firstMsg);
-    ex.message = messages.join('\n');
-
-    return ex;
+  return ex
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,189 +1,169 @@
-"use strict";
+"use strict"
 
-import { expect } from "chai";
+import { expect } from "chai"
 import { create as createDefaultHandlebars } from "handlebars"
-import { escapeJson, createJsonHandlebars } from "../src";
-
+import { escapeJson, createJsonHandlebars } from "../src"
 
 describe("escaping", () => {
+  it("escape enters", () => {
+    // arrange
+    const txt = "This\nis\nso\ncool!"
 
-    it("escape enters", () => {
-        // arrange
-        const txt = "This\nis\nso\ncool!"
+    // act
+    const actual = escapeJson(txt)
 
-        // act
-        const actual = escapeJson(txt);
+    // assert
+    expect(actual).to.equal("This\\nis\\nso\\ncool!")
+  })
 
-        // assert
-        expect(actual).to.equal("This\\nis\\nso\\ncool!");
-    });
+  it("escape quotes", () => {
+    // arrange
+    const txt = 'Einstein: "Uasually I\'m misquoted."'
 
-    it("escape quotes", () => {
+    // act
+    const actual = escapeJson(txt)
 
-        // arrange
-        const txt = 'Einstein: "Uasually I\'m misquoted."';
-
-        // act
-        const actual = escapeJson(txt);
-
-        // assert
-        expect(actual).to.equal("Einstein: \\\"Uasually I\'m misquoted.\\\"");
-    });
-
-});
+    // assert
+    expect(actual).to.equal('Einstein: \\"Uasually I\'m misquoted.\\"')
+  })
+})
 
 describe("create", () => {
+  it("Parse simple JSON template", () => {
+    // arrange
+    const handlebars = createJsonHandlebars()
+    const template = JSON.stringify({ message: "Hello {{name}}!" })
+    const data = { name: "World" }
 
-    it("Parse simple JSON template", () => {
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    const actual = compiledTemplate(data) as any
 
-        // arrange
-        const handlebars = createJsonHandlebars();
-        const template = JSON.stringify({ "message": "Hello {{name}}!" })
-        const data = { name: "World" };
+    // assert
+    expect(actual).not.to.be.null
+    expect(actual.message).to.equal("Hello World!")
+  })
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        const actual = compiledTemplate(data) as any;
+  it("Parse simple JSON template with quotes", () => {
+    // arrange
+    const handlebars = createJsonHandlebars()
+    const template = `{ "message": "Hello {{name}}!" }`
+    const data = { name: 'Quinton "Rampage" Jacksons' }
 
-        // assert
-        expect(actual).not.to.be.null;
-        expect(actual.message).to.equal('Hello World!');
-    });
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    const actual = compiledTemplate(data) as any
 
-    it("Parse simple JSON template with quotes", () => {
+    // assert
+    expect(actual).not.to.be.null
+    expect(actual.message).to.equal('Hello Quinton "Rampage" Jacksons!')
+  })
 
-        // arrange
-        const handlebars = createJsonHandlebars();
-        const template = `{ "message": "Hello {{name}}!" }`;
-        const data = { name: 'Quinton "Rampage" Jacksons' };
+  it("Parse with create", () => {
+    // arrange
+    const handlebars = createJsonHandlebars().create()
+    const template = `{ "message": "Hello {{name}}!" }`
+    const data = { name: 'Quinton "Rampage" Jacksons' }
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        const actual = compiledTemplate(data) as any;
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    const actual = compiledTemplate(data) as any
 
-        // assert
-        expect(actual).not.to.be.null;
-        expect(actual.message).to.equal('Hello Quinton "Rampage" Jacksons!');
-    });
+    // assert
+    expect(actual).not.to.be.null
+    expect(actual.message).to.equal('Hello Quinton "Rampage" Jacksons!')
+  })
 
-    it("Parse with create", () => {
+  it("Default handlebars should not be affected.", () => {
+    // arrange
+    const json = createJsonHandlebars()
+    const standard = createDefaultHandlebars()
+    const template = `{ "message": "Hello {{name}}!" }`
+    const data = { name: 'Quinton "Rampage" Jacksons' }
 
-        // arrange
-        const handlebars = createJsonHandlebars().create();
-        const template = `{ "message": "Hello {{name}}!" }`;
-        const data = { name: 'Quinton "Rampage" Jacksons' };
+    // act
+    const compiledTemplate = standard.compile(template)
+    const actual = compiledTemplate(data) as any
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        const actual = compiledTemplate(data) as any;
-
-        // assert
-        expect(actual).not.to.be.null;
-        expect(actual.message).to.equal('Hello Quinton "Rampage" Jacksons!');
-    });
-
-    it("Default handlebars should not be affected.", () => {
-
-        // arrange
-        const json = createJsonHandlebars()
-        const standard = createDefaultHandlebars();
-        const template = `{ "message": "Hello {{name}}!" }`;
-        const data = { name: 'Quinton "Rampage" Jacksons' };
-
-        // act
-        const compiledTemplate = standard.compile(template);
-        const actual = compiledTemplate(data) as any;
-
-        // assert
-        expect(actual, "Actual should be an object that has HTML escaped strings.").to.equal(`{ "message": "Hello Quinton &quot;Rampage&quot; Jacksons!" }`);
-    });
-
-});
+    // assert
+    expect(actual, "Actual should be an object that has HTML escaped strings.").to.equal(
+      `{ "message": "Hello Quinton &quot;Rampage&quot; Jacksons!" }`
+    )
+  })
+})
 
 describe("error handling", () => {
-
-    it("forget a comma in an arry", () => {
-
-        // arrange
-        const handlebars = createJsonHandlebars();
-        const template = `{
+  it("forget a comma in an arry", () => {
+    // arrange
+    const handlebars = createJsonHandlebars()
+    const template = `{
             "blocks": [
                 {{#items}}
                 { "message": "Hello {{name}}!" }
                 {{/items}}
             ]
-        }`;
-        const data = { items: [{ name: "Alpha" }, { name: "Beta" }] };
+        }`
+    const data = { items: [{ name: "Alpha" }, { name: "Beta" }] }
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        try {
-            compiledTemplate(data);
-        }
-        catch (ex) {
-
-            expect(ex.toString()).to.equal(`SyntaxError: Expected ',' or ']' after array element in JSON at position 88
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    try {
+      compiledTemplate(data)
+    } catch (ex) {
+      expect(ex.toString()).to.equal(`SyntaxError: Expected ',' or ']' after array element in JSON at position 88
 1: {
 2:             "blocks": [
 3:                 { "message": "Hello Alpha!" }
 4:                 { "message": "Hello Beta!" }
 ------------------^
 5:             ]
-6:         }`);
-        }
-    });
+6:         }`)
+    }
+  })
 
-    it("correct for enters", () => {
-
-        const handlebars = createJsonHandlebars();
-        const template = `{
+  it("correct for enters", () => {
+    const handlebars = createJsonHandlebars()
+    const template = `{
             "a": "b"
 
             "c": "d"
-}`;
+}`
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        try {
-            compiledTemplate({});
-        }
-        catch (ex) {
-
-            expect(ex.toString()).to.equal(`SyntaxError: Expected ',' or '}' after property value in JSON at position 35
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    try {
+      compiledTemplate({})
+    } catch (ex) {
+      expect(ex.toString()).to.equal(`SyntaxError: Expected ',' or '}' after property value in JSON at position 35
 1: {
 2:             "a": "b"
 3:             "c": "d"
 --------------^
-4: }`);
-        }
-    });
-
-});
+4: }`)
+    }
+  })
+})
 
 describe("parsing of enters", () => {
-
-    it("replace enters", () => {
-
-        // arrange
-        const handlebars = createJsonHandlebars();
-        const template = `{
+  it("replace enters", () => {
+    // arrange
+    const handlebars = createJsonHandlebars()
+    const template = `{
   "a": "{{name}}"
 
   ,
 
   "b": "{{name}}"
-}`;
-        const data = { name: "World" };
+}`
+    const data = { name: "World" }
 
-        // act
-        const compiledTemplate = handlebars.compile(template);
-        const actual = compiledTemplate(data) as any;
+    // act
+    const compiledTemplate = handlebars.compile(template)
+    const actual = compiledTemplate(data) as any
 
-        // assert
-        expect(actual).not.to.be.null;
-        expect(actual.a).to.equal(data.name);
-        expect(actual.b).to.equal(data.name);
-
-    });
-
-});
+    // assert
+    expect(actual).not.to.be.null
+    expect(actual.a).to.equal(data.name)
+    expect(actual.b).to.equal(data.name)
+  })
+})
