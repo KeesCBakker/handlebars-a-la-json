12de2e4cde818eb53c49cffa811ce259b489f7ac
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2024-12-09 10:12:26 +0100

    Test error handling cause.

--- test/files/NamedTemplateParser.spec.ts
+++ test/files/NamedTemplateParser.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "chai"
 import { create as createDefaultHandlebars } from "handlebars"
 import { NamedTemplateParser } from "../../src/files/NamedTemplateParser"
 import { createJsonHandlebars } from "../../src/templating/createJsonHandlebars"
+import fs from "fs"
 
 describe("files/NamedTemplateParser.spec.ts", () => {
   it("Parse directory async", async () => {
@@ -49,5 +50,32 @@ describe("files/NamedTemplateParser.spec.ts", () => {
     // assert
     expect(actual.count).to.eq(2)
     expect(actual.list).to.deep.eq(["-Alpha", "-Beta"])
+  })
+
+  it("Invalid home", async () => {
+    // arrange
+    const handlebars = createJsonHandlebars()
+    const parser = new NamedTemplateParser(handlebars)
+    const template = await fs.promises.readFile(__dirname + "/../files/templates/broken-home.handlebars")
+
+    parser.addNamedTemplate("broken-home", template.toString())
+
+    // act
+    try {
+      parser.parse("broken-home", {})
+    } catch (ex) {
+      // assert
+      expect(ex.message).to.eq("Template invalid: broken-home")
+      expect(ex.cause.message).to.eq(`Expected ',' or '}' after property value in JSON at position 434
+17:             "type": "plain_text",
+18:             "text": ":Provision Database"
+19:           }
+20:         }
+21:       ]dd
+-----------^
+22:     }
+23:   ]
+24: }`)
+    }
   })
 })
